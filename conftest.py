e31a2a12ad9a6210c785f5e60f8e8b49ce487c17
Long Vu <vu.long@ouranos.ca>
2021-05-18 14:55:25 -0400

    test: good fix to ignore by default html and javascript output to decrease usage of NBVAL_IGNORE_OUTPUT
    Fix the error found in the previous commit 36d2fb42fcea81e2d5b14034d10298ec51bb7d08.

--- conftest.py
+++ conftest.py
@@ -1,2 +1,3 @@
 def pytest_collectstart(collector):
-    collector.skip_compare += 'text/html', 'application/javascript',
+    if collector.fspath and collector.fspath.ext == '.ipynb':
+        collector.skip_compare += 'text/html', 'application/javascript',
