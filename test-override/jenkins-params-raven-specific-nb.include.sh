3ae15dc07102f9d33fff2842a624312b0cae3672
Long Vu <vu.long@ouranos.ca>
2024-05-09 23:52:31 -0400

    jenkins override sample: improve explanation in comment

--- test-override/jenkins-params-raven-specific-nb.include.sh
+++ test-override/jenkins-params-raven-specific-nb.include.sh
@@ -14,13 +14,14 @@ cat test-override/jenkins-params-raven-nb-only.include.sh
 
 . test-override/jenkins-params-raven-nb-only.include.sh
 
-# Then we create CONFIG_OVERRIDE_SCRIPT_URL file on-the-fly.
+# Then we create CONFIG_OVERRIDE_SCRIPT_URL file on-the-fly to filter for the 2
+# notebooks that we want.
 
 CONFIG_OVERRIDE_SCRIPT_URL="/tmp/specific-raven-nb.include.sh"
 
 echo '
 #!/bin/sh
-# Sample config override script to only run 2 raven notebooks.
+# Sample config override script to only run 2 specific raven notebooks.
 
 NEW_NB_LIST=""
 for nb in $NOTEBOOKS; do
